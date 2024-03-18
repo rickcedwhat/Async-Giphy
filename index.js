1bfd14e5626ea195b4534b44bf03f1775b204767
Harpole <mharpole@deloitte.com>
2024-03-18 15:23:50 -0400

    index.js code

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-require('dotenv').config();
+require('.env').config();
+
+async function getImage(query) {
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${.env}&q=${encodeURIComponent(query)}&limit=1&offset=0&rating=g&lang=en`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    const imageUrl = data.data[0].images.original.url;
+    console.log(imageUrl);
+    return imageUrl;
+}
 
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
